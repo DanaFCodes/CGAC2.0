593665c09ae07fa1003ce313cb46de6fe679f151
Dana Filatov <dana.filatov@pts-dfil.local>
2023-09-25 15:47:46 -0400

    footer

--- components/Foot.js
+++ components/Foot.js
@@ -19,10 +19,10 @@ function Foot() {
         alt="loop divider"
         />
         
-        <h3 className="mt-8 text-3xl font-bod w-[100%] sm:text-center md:text-start">➰follow us on Instagram➰</h3>
+        <h3 className="mt-8 md:text-3xl text-xl font-bod w-[100%] text-center md:text-start">➰follow us on Instagram➰</h3>
            
         <div className="instaImages flex mt-6 w-[100%] justify-start">
-          <div className="mr-10"> 
+          <div className="md:mr-10 mr-4"> 
             <Image
             src={'/poster.png'}
             width={400}
@@ -30,7 +30,7 @@ function Foot() {
             alt="CGAC workshop poster"
             />
           </div>
-          <div className="mr-10">
+          <div className="md:mr-10 mr-4">
             <Image
             src={'/insta(1).png'}
             width={530}
@@ -50,33 +50,33 @@ function Foot() {
 
         <div className="flex justify-between flex-wrap" id='bottomDiv'>
           <div id='contactForm' className="mt-16 sm:content-center">
-            <h3 className="text-3xl font-bod  sm:text-center md:text-start">➰stay in the loop➰</h3>
+            <h3 className="md:text-3xl text-xl font-bod text-start">➰stay in the loop➰</h3>
               <form className="footerInput pt-4 flex" action="">
                 <label htmlFor="submit"></label>
                 <input
                   name="submit"
                   type="text required"
-                  placeholder="gardenartist@gmail.com  "
+                  placeholder="gardenart@gmail.com  "
                   id='formInput'
-                  className="rounded-sm h-12 font-bod pl-2 pr-10 sm:content-center   "
-                  />
+                  className="rounded-sm h-12 font-bod pl-2 pr-10 sm:w-64 content-center"
+                />
               </form>
             <button id='formButton' className="btn btn-animation font-bod  mt-5 ">Submit</button>
           </div>
 
           <div id='contactUs' className=" justify-between">
-            <h3 className="text-3xl font-bod flex md:justify-end mt-16  ">➰contact us➰</h3>
-              <div className="flex items-center sm:justify-start md:justify-end md:mr-3 mt-5">
+            <h3 className="md:text-3xl text-xl font-bod flex justify-end mt-16">➰contact us➰</h3>
+              <div className="flex items-center sm:justify-start justify-end md:mr-3 mt-5">
                 <AiFillInstagram className="scale-150 mr-2 sm:justify-start" />
                 <p>@cgac.workshop</p>
               </div>
 
-              <div className="flex items-center sm:justify-start md:justify-end md:mr-3 mt-5">
+              <div className="flex items-center sm:justify-start justify-end md:mr-3 mt-5">
                 <FaTiktok className="scale-150 mr-2" />
                 <p>@cgac.toronto</p>
               </div>
 
-              <div className="flex items-center sm:justify-start md:justify-end md:mr-3 mt-1">
+              <div className="flex items-center sm:justify-start justify-end md:mr-3 mt-1">
                 <AiOutlineMail className="scale-150 mt-3 mr-2" />
                 <p className="mt-3">cgac.spillthebeans@gmail.com</p>
               </div>
@@ -94,7 +94,7 @@ function Foot() {
         </div>
             
         <div className="flex items-center justify-center">
-          <p className="text-xs text-center font-bod mt-1">
+          <p className="text-xs sm:text-sm text-center font-bod mt-1">
             Website by Dana Filatov &copy; 2023 Community Garden Art Collective
             (CGAC)
           </p>
